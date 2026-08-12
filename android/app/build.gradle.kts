a1f869bb59e3554333cd32169a988d9f5731f2b2
PaxSat <pasquale.cardillo@gmail.com>
2026-08-12 12:28:53 +0200

    rinominato package definitivo in it.paxsat.rdebridge

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.ridebridge.ridebridge"
+    namespace = "it.paxsat.ridebridge"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -18,7 +18,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.ridebridge.ridebridge"
+        applicationId = "it.paxsat.ridebridge"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
